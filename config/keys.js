2c076a18611333aeede4d86ed3a29fa10ebab452
Mark <s3venvials@yahoo.com>
2018-12-01 12:03:42 -0700

    Implemented storing files to mongoDB with multer-gridfs-storage.

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
       mongo: {
-        dbURI: "mongodb://admin:admin123@ds041167.mlab.com:41167/docmanagerv1"
+        dbURI: ""
       }
 };
--- routes/document_manager.js
+++ routes/document_manager.js
@@ -1,23 +1,41 @@
-let express = require("express");
-let fs = require('fs');
-let path = require('path');
-let router = express.Router();
-let pathToDocs = path.join(__dirname, '../');
-let multer  = require('multer');
-let mongoose = require("mongoose");
-let gridFsStorage = require("multer-gridfs-storage");
-let gridStream = require("gridfs-stream");
-let keys = require("../config/keys");
-
-let gfs;
-let conn = mongoose.createConnection(keys.mongo.dbURI, { useNewUrlParser: true })
-
+const express = require("express");
+const router = express.Router();
+const multer  = require('multer');
+const mongoose = require("mongoose");
+const gridFsStorage = require("multer-gridfs-storage");
+const gridStream = require("gridfs-stream");
+const keys = require("../config/keys");
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const crypto = require("crypto");
+router.use(bodyParser.json());
+router.use(methodOverride('_method'));
 //==============
 //Modules
 //==============
 let getTime  = require("../modules/curr_date").curTime;
 let getDate  = require("../modules/curr_date").curDate;
-// let getError = require("../modules/exception_viewer").newError;
+
+let conn = mongoose.createConnection(keys.mongo.dbURI, { useNewUrlParser: true })
+let gfs;
+
+conn.once('open', (req, res) => {
+    gfs = gridStream(conn.db, mongoose.mongo);
+    gfs.collection('uploads');
+});
+
+const storage = new gridFsStorage({
+    url: keys.mongo.dbURI,
+    file: (req, file) => {
+        return {
+            filename: file.originalname,
+            bucketName: 'uploads',
+        }
+    }
+});
+const upload = multer({
+     storage: storage
+}).single('file');
 
 router.get("/api", (req, res) => {
     gfs.files.find().toArray((err, files) => {
@@ -43,79 +61,11 @@ router.get("/upload", (req ,res) => {
     res.render("upload");
 });
 
-conn.once('open', (req, res) => {
-    gfs = gridStream(conn.db, mongoose.mongo);
-    gfs.collection('uploads');
-});
-
-const storage = new gridFsStorage({
-    url: keys.mongo.dbURI,
-    file: ( req, file) => {
-        return new Promise((resolve, reject) => {
-            const fileName = file.originalname + path.extname(file.originalname);
-            const fileInfo = {
-                filename: fileName,
-                bucketname: 'uploads'
-            };
-            resolve(fileInfo);
-        });
-    }
-});
-
-const upload = multer({
-    storage: storage
-    // fileFilter: function (req, file, cb) {
-    //     checkFileType(file, cb);
-    // }
-}).single('file');
-//User .array for uploading multiple files.
-
-// USE FOR LOCAL STORAGE
-// const storage = multer.diskStorage({
-//     destination: 'public/files',
-//     filename: function(req, file, cb) {
-//         cb(null, file.originalname);
-//     }
-// });
-
-async function checkFileType(file, cb) {
-    // Allowed file ext
-    // const fileTypes = /txt|docx|xlsx|pdf|text/;
-    const fileTypes = ['.txt', '.docx', '.pdf', '.xlsx', 'text'];
-    
-    // console.log(fileTypes.test('test.text'));
-    // Check file ext
-    const extname = path.extname(file.originalname).toLowerCase();
-    // check mimetype
-    // const mimetype = fileTypes.test(file.mimetype);
-    console.log(file.mimetype);
-    let promise = new Promise((resolve, reject) => {
-        fs.readdir(`${pathToDocs}/public/files/`, (err, files) => {
-        if(err){return reject(err)};
-        if(files) return resolve(files);
-        });
-    });
-    let results = await promise;
-    if(fileTypes.includes(extname) && !results.includes(file.originalname)) {
-        return cb(null, true);
-    } else {
-        cb('Error: Invalid error');
-    }
-};
-
-function checkExistingFile() {
-    fs.readdir(`${pathToDocs}/public/files/`, (err, files) => {
-        for(var i = 0; i < files.length; i++) {
-            return files;
-        }
-    });
-}
-
 router.post("/", (req, res) => {
     upload(req, res, (err) => {
         if(err){
             console.log(err);
-            req.flash('error', 'Error uploading your file. Invalid or duplicate file type was detected.');
+            req.flash('error', 'Error uploading your file. Server error, invalid or duplicate file was detected.');
             res.redirect('back');
         } else {
             req.flash('success', `File ${req.file.originalname} was uploaded successfully!`)
@@ -124,17 +74,28 @@ router.post("/", (req, res) => {
     });
 });
 
-router.post('/delete', (req, res) => {
-    let file = req.body.file;
-    fs.unlink(`${pathToDocs}/public/files/${file}`, (err, files) => {
-        if(err) {
-            req.flash('error', JSON.stringify(err));
-            res.redirect('back');
-        } else {
-            req.flash('info', `File ${file} was deleted successfully!`);
-            res.redirect('back');
-        }
-   });
-});
+router.get('/files/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+      // File exists
+      return res.json(file);
+    });
+  });
+
+router.delete('/files/:id', (req, res) => {
+    let filename = req.body.filename;
+    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+      if (err) {
+        return res.status(404).json({ err: err });
+      }
+      req.flash('success', filename + " was successfully deleted.");
+      res.redirect('/');
+    });
+  });
 
 module.exports = router;
